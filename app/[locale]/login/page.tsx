8eecd8207abec35d46bbc0d49d6abf62ea1fae93
Netra Luis Castells <netraluis@gmail.com>
2025-06-11 10:21:48 +0200

    add logo globalrisc

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -15,21 +15,13 @@ export default function LoginPage() {
   return (
     <div className="container relative flex min-h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
-        <div className="absolute inset-0 bg-zinc-900" />
+        <div className="absolute inset-0 bg-[#84C9E5]" />
         <div className="relative z-20 flex items-center text-lg font-medium">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="mr-2 h-6 w-6"
-          >
-            <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-          </svg>
-          {t('company')}
+          <img
+            src="/LOGO_GLOBALRISC_web.svg"
+            alt="Globalrisc Logo"
+            className="mr-2 h-6 w-auto"
+          />
         </div>
         <div className="relative z-20 mt-auto">
           <blockquote className="space-y-2">
